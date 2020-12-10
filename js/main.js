7686d4a31331f02bc59f40e766c3c525a5cdd687
tomoyuki_oho <>
2020-12-10 15:49:46 +0900

    アイコンをカテゴリごとに一覧表示するようにした。

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 $(function() {
   'use strict';
 
-  let icons = [];
+  let icons = {};
 
   $.getJSON('./data/icons.json')
   // 2. doneは、通信に成功した時に実行される
   //  引数のdataは、通信で取得したデータ
   .done(function(data,textStatus,jqXHR) {
-    data["data"].forEach((icon) => {
-      icons.push(icon);
-    });
+    // data["data"].forEach((icon) => {
+    //   icons.push(icon);
+    // });
+    icons = data;
     draw();
   })
   // 5. failは、通信に失敗した時に実行される
@@ -18,11 +19,28 @@ $(function() {
   });
   
   function draw() {
-    icons.forEach((icon) => {
-      const i = document.createElement("i");
-      i.classList.add("fas");
-      i.classList.add(icon);
-      document.body.appendChild(i);
-    });
+    // console.log(icons);
+    let obj;
+    for(let category in icons) {
+      const container = document.createElement("div");
+      container.classList.add('container');
+      document.body.appendChild(container);
+
+      const categoryName = document.createElement("div");
+      categoryName.innerText = category;
+      container.appendChild(categoryName);
+
+      const iconContainer = document.createElement("div");
+      iconContainer.classList.add('icon-container');
+      container.appendChild(iconContainer);
+
+      const iconNames = icons[category];
+      iconNames.forEach(iconName => {
+        const i = document.createElement("i");
+        i.classList.add("fas");
+        i.classList.add(iconName);
+        iconContainer.appendChild(i);
+      });
+    }
   }
 });
